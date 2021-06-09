39734174a3f47716a8a40b1110dd9e9e01331087
edokristian05 <80260445+edokristian05@users.noreply.github.com>
2021-06-09 20:45:30 +0700

    menambahkan praktikum API Volley

---
+++ Praktikum API Volley/APIVolley/app/src/main/java/com/example/apivolley/Adapter/AdapterData.java
@@ -0,0 +1,91 @@
+package com.example.apivolley.Adapter;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.apivolley.InsertData;
+import com.example.apivolley.Model.ModelData;
+import com.example.apivolley.R;
+
+import java.util.List;
+
+public class AdapterData extends RecyclerView.Adapter<AdapterData.HolderData> {
+    private List<ModelData> mItems;
+    private Context context;
+
+    /**
+     * Deklarasi untuk menampung banyak data
+     */
+    public AdapterData (Context context, List<ModelData> items)
+    {
+        this.mItems = items;
+        this.context = context;
+    }
+    /**
+     * Deklarasi layout_row yang menampung tiap data
+     */
+    @Override
+    public HolderData onCreateViewHolder(ViewGroup parent, int viewType) {
+        View layout = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_row,parent,
+                false);
+        HolderData holderData = new HolderData(layout);
+        return holderData;
+    }
+    /**
+     * Deklarasi tiap variabel yang menampung data sesuai dengan data di database
+     */
+    @Override
+    public void onBindViewHolder(HolderData holder, int position) {
+        ModelData md = mItems.get(position);
+        holder.tvusername.setText(md.getUsername());
+        holder.tvgrup.setText(md.getGrup());
+        holder.tvnama.setText(md.getNama());
+        holder.tvpassword.setText(md.getPassword());
+        holder.md = md;
+    }
+    /**
+     * Menghitung banyaknya data yang diambil
+     */
+    @Override
+    public int getItemCount() { return mItems.size(); }
+    /**
+     * Membuat class HolderData turunan dari RecyclerView.ViewHolder
+     */
+    class HolderData extends RecyclerView.ViewHolder
+    {
+        TextView tvusername, tvgrup, tvnama, tvpassword;
+        ModelData md;
+        /**
+         * Deklarasikan setiap widget yang ada di layout_row
+         */
+        public HolderData (View view)
+        {
+            super(view);
+            tvusername = (TextView) view.findViewById(R.id.username);
+            tvgrup = (TextView) view.findViewById(R.id.grup);
+            tvnama = (TextView) view.findViewById(R.id.nama);
+            tvpassword = (TextView) view.findViewById(R.id.password);
+            view.setOnClickListener(new View.OnClickListener() {
+                /** Apabila salah satu data di klik maka akan menampilkan detail data*/
+                @Override
+                public void onClick(View view) {
+                    Intent update = new Intent(context, InsertData.class);
+                    update.putExtra("update", 1);
+                    update.putExtra("username", md.getUsername());
+                    update.putExtra("grup", md.getGrup());
+                    update.putExtra("nama", md.getNama());
+                    update.putExtra("passwprd", md.getPassword());
+
+                    context.startActivity(update);
+                }
+            });
+        }
+    }
+}
---
+++ Praktikum API Volley/APIVolley/app/src/main/java/com/example/apivolley/Util/AppController.java
@@ -0,0 +1,59 @@
+package com.example.apivolley.Util;
+
+import android.app.Application;
+import android.text.TextUtils;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+public class AppController extends Application {
+    private static final String TAG = AppController.class.getSimpleName();
+    private static AppController instance ;
+    RequestQueue mRequestQueue;
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        instance = this;
+    }
+    public static synchronized AppController getInstance() {return instance; }
+
+    /**
+     * Mencoba mengantri untuk mengambil data melalui API
+     */
+    private RequestQueue getRequestQueue()
+    {
+        if(mRequestQueue == null)
+        {
+            mRequestQueue = Volley.newRequestQueue(getApplicationContext());
+        }
+        return mRequestQueue;
+    }
+    /**
+     * Memproses untuk mengambil data
+     */
+    public <T> void addToRequestQueue(Request<T> req, String tag)
+    {
+        req.setTag(TextUtils.isEmpty(tag) ? TAG : tag);
+        getRequestQueue().add(req);
+    }
+    /**
+     * Menampilkan data yang telah diambil melalui API
+     */
+    public <T> void addToRequestQueue (Request<T> req)
+    {
+        req.setTag(TAG);
+        getRequestQueue().add(req);
+    }
+    /**
+     * Membatalkan semua proses pengambilan data
+     */
+    public void cancelAllRequest(Object req)
+    {
+        if (mRequestQueue != null)
+        {
+            mRequestQueue.cancelAll(req);
+        }
+    }
+}
